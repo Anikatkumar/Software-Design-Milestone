6188c6d042d76b263449e7c5b75aa1c8e7d4a9f7
AngelaNrqz11 <angelaanneenriquez@gmail.com>
2024-09-29 20:06:58 +1000

    Enable Splash Screen

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,19 +1,13 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
-import settings.GameSettings;
 import ui.MainMenuScreen;
 import ui.SplashScreen;
 
 import javax.swing.*;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-//        SplashScreen splash = new SplashScreen(100);
+        SplashScreen splash = new SplashScreen(1000);
         MainMenuScreen mainMenu = new MainMenuScreen();
-//        splash.showSplash();
+        splash.showSplash();
         SwingUtilities.invokeLater(mainMenu::showMainScreen);
     }
 }
